119cacd81750b29d97888d2723869809de9e53c2
tobiwashi <tobiigberase@gmail.com>
2023-08-23 14:32:35 -0400

    Data pulled from Firebase

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
-const ImgSLider = (props) => {
+const ImgSlider = (props) => {
     let settings = {
         dots: true,
         infinite: true,
@@ -14,33 +14,94 @@ const ImgSLider = (props) => {
 
     return (
         <Carousel {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
+            <Wrap>
+                <a href='/'>
+                    <img src='/images/slider-badging.jpg' alt='Slider Img'></img>
+                </a>
+            </Wrap>
+            <Wrap>
+                <a href='/'>
+                    <img src='/images/slider-scale.jpg' alt='Slider Img'></img>
+                </a>
+            </Wrap>
+            <Wrap>
+                <a href='/'>
+                    <img src='/images/slider-badag.jpg' alt='Slider Img'></img>
+                </a>
+            </Wrap>
+            <Wrap>
+                <a href='/'>
+                    <img src='/images/slider-scales.jpg' alt='Slider Img'></img>
+                </a>
+            </Wrap>
         </Carousel>
     )
 }
 
 const Carousel = styled(Slider)`
 margin-top: 20px;
-overflow: hidden;
+
 
 & > button {
     opacity: 0;
     height: 100%;
     width: 5vw;
     z-index: 1;
-}
-&: hover {
+
+ &:hover {
     opacity: 1;
-    height: 100%
+    transition: opacity .02s ease 0s;
+    }
+ }
+ 
+ ul li button {
+
+    &:before{
+        font-size: 10px;
+        color: rgb(150, 158, 171);
+    }
+ }
+
+ li.slick-active button:before {
+    color: white;
+ }
+
+ .slick-list {
+    overflow: initial;
+ }
+
+ .slick-prev {
+    left: -75px;
+ }
+
+ .slick-nextr {
+    right: -75px;
+ }
+`
+const Wrap = styled.div`
+border-radius: 4px;
+cursor: pointer;
+position: relative;
+
+a {
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 38px --10px, 
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    cursor: pointer;
+    display: block;
+    position: relativ;
+    padding: 4px;
 }
 
+img {
+    width: 100%;
+    height: 100%;
+}
+
+&:hover {
+    padding: 0;
+    border: 4px solid white;
+    transition-durations: 300ms;
+}
 `
-export default ImgSLider
+export default ImgSlider
